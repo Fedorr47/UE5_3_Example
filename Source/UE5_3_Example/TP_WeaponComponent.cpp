90448dc57a4a12e6dfbbe68620495c553a1a1df1
Fedorr47 <fedorr47@gmail.com>
2024-03-21 16:58:11 +0100

    Modified assets

--- Source/UE5_3_Example/TP_WeaponComponent.cpp
+++ Source/UE5_3_Example/TP_WeaponComponent.cpp
@@ -76,6 +76,7 @@ void UTP_WeaponComponent::AttachWeapon(AUE5_3_ExampleCharacter* TargetCharacter)
 	// Attach the weapon to the First Person Character
 	FAttachmentTransformRules AttachmentRules(EAttachmentRule::SnapToTarget, true);
 	AttachToComponent(Character->GetMesh1P(), AttachmentRules, FName(TEXT("GripPoint")));
+
 	
 	// switch bHasRifle so the animation blueprint can switch to another animation set
 	Character->SetHasRifle(true);
